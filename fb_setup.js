2c7382ebdd0e839b6e2798ce50f27e8acb69befa
Sassysiren274 <25471jf@hvhs.school.nz>
2026-04-23 21:57:56 +0000

    fgb

--- fb_setup.js
+++ fb_setup.js
@@ -7,15 +7,17 @@
 // Input:  n/a
 // Return: n/a
 /**************************************************************/
-  const firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    databaseURL: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId:,
-    appId:
-  };
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeKv0lyJAyYeDWhctZZQtNmjRvQmTd1U0",
+  authDomain: "jalwa-faisal-12comp.firebaseapp.com",
+  databaseURL: "https://jalwa-faisal-12comp-default-rtdb.firebaseio.com",
+  projectId: "jalwa-faisal-12comp",
+  storageBucket: "jalwa-faisal-12comp.firebasestorage.app",
+  messagingSenderId: "248210801474",
+  appId: "1:248210801474:web:11e1b6b42ed36afeae7edb",
+  measurementId: "G-Z4D9LRQMGS"
+};
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
